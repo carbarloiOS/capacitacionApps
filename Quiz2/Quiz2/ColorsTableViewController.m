b5a0dda06191e7d9cea8aa2fd83a87cbe5087ca3
Estudiantes <Estudiantes@Mac-mini-de-Estudiantes-10.local>
2017-02-11 10:22:14 -0600

    added quiz 2

---
+++ Quiz2/Quiz2/ColorsTableViewController.m
@@ -0,0 +1,123 @@
+//
+//  ColorsTableViewController.m
+//  Quiz2
+//
+//  Created by Estudiantes on 11/2/17.
+//  Copyright © 2017 Universidad Estatal a Distancia Costa Rica. All rights reserved.
+//
+
+#import "ColorsTableViewController.h"
+
+@interface ColorsTableViewController ()
+
+@end
+
+@implementation ColorsTableViewController
+
+
+
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 5000;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell" forIndexPath:indexPath];
+    CGFloat modulo = indexPath.row % 2;
+    if (modulo > 0) {
+        cell.backgroundColor = [UIColor blueColor];
+    }
+    else {
+        cell.backgroundColor = [UIColor redColor];
+    }
+    long numero = indexPath.row;
+    cell.textLabel.text= [NSString stringWithFormat:@"%ld", numero];
+    
+    /*
+    int i;
+    
+    for (i = 0; i < 5000; i++) {
+        CGFloat modulo = i % 2;
+        if (modulo > 0) {
+            cell.backgroundColor = [UIColor redColor];
+        }
+        else {
+            cell.backgroundColor = [UIColor blueColor];
+        }
+        cell.textLabel.text= [NSString stringWithFormat:@"%i", i];
+    // Configure the cell...
+    } // for*/
+    return cell;
+}
+
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
